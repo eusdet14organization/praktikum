82af6ab19d1cd1d7fba223d9eb2c68a0f0665d51
Denys Nazarov <div.itcor@gmail.com>
2024-12-18 23:55:10 +0100

    Fixed locators on xpath. Three tests are ready that cover the first requirement in the user story

--- src/test/java/Praktikum14Cucumber/pages/MyAccountPage.java
+++ src/test/java/Praktikum14Cucumber/pages/MyAccountPage.java
@@ -6,13 +6,16 @@ import org.openqa.selenium.support.FindBy;
 
 public class MyAccountPage extends BasePage {
     //Returning Customer page
-    @FindBy(xpath = "#Email")
+    @FindBy(xpath = "//h1[@class='h3 text-light mb-0']")
+    public WebElement titleH1;
+
+    @FindBy(xpath = "//*[@id='Email']")
     public WebElement eMail;
 
-    @FindBy(xpath = "#Password")
+    @FindBy(xpath = "//*[@id='Password']")
     public WebElement password;
 
-    @FindBy(xpath = "#RememberMe")
+    @FindBy(xpath = "//*[@id='RememberMe']")
     public WebElement rememberMe;
 
     @FindBy(xpath = "//div[@class='d-flex flex-wrap justify-content-between']/a")
--- src/test/java/Praktikum14Cucumber/steps/RegistrationFormSteps.java
+++ src/test/java/Praktikum14Cucumber/steps/RegistrationFormSteps.java
@@ -1,19 +1,71 @@
 package Praktikum14Cucumber.steps;
 
+import Praktikum14Cucumber.context.TestContext;
 import Praktikum14Cucumber.pages.HomePage;
+import Praktikum14Cucumber.pages.MyAccountPage;
+import Praktikum14Cucumber.pages.RegistrationFormPage;
+import Praktikum14Cucumber.utils.ConfigurationReader;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RegistrationFormSteps {
 
     HomePage homePage =new HomePage();
+    MyAccountPage myAccountPage = new MyAccountPage();
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
-    @And("The user click on the \"Mein Konto\" in the main menu")
-    public void theUserClickOnTheMMeinKontoInTheMainMenu() {
-        homePage.getMeinMenu("Mein Konto");
+    @And("The user click on the {string} in the main menu")
+    public void theUserClickOnTheMMeinKontoInTheMainMenu(String nameMenu) {
+        homePage.getMeinMenu(nameMenu).click();
     }
 
+    @And("The user click on the \"Hallo Konto\" menu item")
+    public void theUserClickOnTheMenuItem() {
+        homePage.loginUser.click();
+    }
 
+    @And("The user click on the {string} menu item in the footer")
+    public void theUserClickOnTheMenuItemInTheFooter(String nameFooterMenu) {
+        homePage.getFooterMenu(nameFooterMenu).click();
+    }
+
+    @When("The user click on the Register button")
+    public void theUserClickOnTheButton() {
+        myAccountPage.registerButton.click();
+    }
+
+    @Then("The user should be redirected to the registration page")
+    public void theUserShouldBeRedirectedToTheRegistrationPage() {
+        assertEquals(TestContext.getDriver().getCurrentUrl(),
+                ConfigurationReader.get("logInUrl"));
+        assertEquals(TestContext.getDriver().getTitle(),
+                "Dr Müller. Registrierung");
+    }
+
+    @And("The name of registration form should be displayed")
+    public void theNameOfRegistrationFormShouldBeDisplayed() {
+        assertEquals(registrationFormPage.titleH1.getText(),
+                "Registrierung");
+    }
+
+    @And("The form should include the following fields")
+    public void theFormShouldIncludeTheFollowingFields(List<String> namesOfField) {
+        registrationFormPage.verifyFormFields(namesOfField);
+        assertTrue(registrationFormPage.firstName.isDisplayed());
+        assertTrue(registrationFormPage.lastName.isDisplayed());
+        assertTrue(registrationFormPage.eMail.isDisplayed());
+        assertTrue(registrationFormPage.password.isDisplayed());
+        assertTrue(registrationFormPage.confirmPassword.isDisplayed());
+    }
+
+    @And("The form should include a Register button")
+    public void theFormShouldIncludeAButton() {
+        assertTrue(registrationFormPage.registerButton.isDisplayed());
+    }
 }
