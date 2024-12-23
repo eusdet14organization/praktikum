f5b97eba72d614867ee2264e75c3643decd0cdc1
Oleh Herasymenko <geradimona@gmail.com>
2024-12-23 11:26:29 +0100

    first localisation scenario

--- src/test/java/Praktikum14Cucumber/steps/LocalizationSteps.java
+++ src/test/java/Praktikum14Cucumber/steps/LocalizationSteps.java
@@ -18,7 +18,7 @@ public class LocalizationSteps {
 
     @When("I hover the mouse over the Language switcher")
     public void iHoverTheMouseOverTheLanguageSwitcher() {
-        TestContext.getWait().until(ExpectedConditions.elementToBeClickable(homePage.switchLanguageEN));
+       // TestContext.getWait().until(ExpectedConditions.elementToBeClickable(homePage.switchLanguageEN));
         TestContext.getActions().moveToElement(homePage.switchLanguageEN).perform();
     }
 
@@ -31,6 +31,7 @@ public class LocalizationSteps {
     }
 
 
+
     @When("I move the cursor over the English language icon")
     public void iMoveTheCursorOverTheEnglishLanguageIcon() {
         TestContext.getActions().moveToElement(homePage.switchLanguageEN).perform();
@@ -39,17 +40,22 @@ public class LocalizationSteps {
     @Then("the Tooltip {string} is displayed below the English  language button")
     public void theTooltipIsDisplayedBelowTheEnglishLanguageButton(String arg0) {
         TestContext.getWait().until(ExpectedConditions.visibilityOf(localizationPage.englishLanguageTitle));
-        assertEquals("EN", localizationPage.englishLanguageTitle.getText(), "Tooltip text is not correct");
+        assertEquals("EN", localizationPage.englishLanguageTitle.getAttribute("title"), "Tooltip text is not correct");
     }
+
+
 
     @When("I move the cursor over the German language icon")
     public void iMoveTheCursorOverTheGermanLanguageIcon() {
-
+        TestContext.getActions().moveToElement(homePage.switchLanguageDE).perform();
     }
 
     @Then("the Tooltip {string} is displayed below the German button")
     public void theTooltipEdIsDisplayedBelowTheGermanButton() {
+        TestContext.getWait().until(ExpectedConditions.visibilityOf(localizationPage.englishLanguageTitle));
+        assertEquals("DE", localizationPage.germanLanguageTitle.getAttribute("title"), "Tooltip text is not correct");
     }
+
 
 
     @When("I click on the German language icon")
@@ -61,12 +67,12 @@ public class LocalizationSteps {
     public void allTextOnTheSiteIsDisplayedInGerman() {
         String languageText = localizationPage.startSeite.getText();
         assertTrue(languageText.contains("Startseite"), "Site text is not in German");
+        System.out.println(languageText.contains("Startseite"));
     }
-
 
     @Then("all texts on the website should be displayed in German")
     public void allTextsOnTheWebsiteShouldBeDisplayedInGerman() {
-        TestContext.getWait().until(ExpectedConditions.visibilityOf(localizationPage.startSeite));
+        //TestContext.getWait().until(ExpectedConditions.visibilityOf(localizationPage.startSeite));
         String actualText = localizationPage.startSeite.getText();
         assertTrue(actualText.contains("Startseite"),"The text is not in German");
     }
@@ -76,11 +82,11 @@ public class LocalizationSteps {
         TestContext.getDriver().navigate().refresh();
     }
 
-
     @Then("all texts on the website should still be displayed in German")
     public void allTextsOnTheWebsiteShouldStillBeDisplayedInGerman() {
-        TestContext.getWait().until(ExpectedConditions.visibilityOf(localizationPage.startSeite));
+        //TestContext.getWait().until(ExpectedConditions.visibilityOf(localizationPage.startSeite));
         String actualText = localizationPage.startSeite.getText();
         assertTrue(actualText.contains("Startseite"),"The text is not in German");
     }
+
 }
--- src/test/resources/features/Lokalization.feature
+++ src/test/resources/features/Lokalization.feature
@@ -4,29 +4,25 @@ Feature: Language Switcher
   Background:
     Given The user is on the main page
 
-#  Scenario: Hover over Language Switcher
-#    When I hover the mouse over the Language switcher
-#    Then I see a choose of two languages with country flags: English and German
+  Scenario: Hover over Language Switcher
+    When I hover the mouse over the Language switcher
+    Then I see a choose of two languages with country flags: English and German
 
 #  Scenario: Tooltip for English language
 #    When I move the cursor over the English language icon
 #    Then the Tooltip "En" is displayed below the English  language button
-
+#
 #  Scenario: Tooltip for German language
 #    When I move the cursor over the German language icon
 #    Then the Tooltip 'DE' is displayed below the German button
 
-#  Scenario: Switch to German language
-#    When I click on the German language icon
-#    Then all text on the site is displayed in German
-
-#  Scenario: Switch to English language
-#    When I click on the English language icon
-#    Then all text on the site is displayed in English
-
+  Scenario: Switch to German language
     When I click on the German language icon
+    Then all text on the site is displayed in German
     Then all texts on the website should be displayed in German
     When I reload the page
     Then all texts on the website should still be displayed in German
+
+
 
 
