27951afa39e2caf5fbcf7dbf9abd591dacbf0081
Oleh Herasymenko <geradimona@gmail.com>
2025-01-02 13:11:25 +0100

    it was added Contact Page and 3 Scenarios шт Contactfunktionality

---
+++ src/test/java/Praktikum14Cucumber/pages/ContactPage.java
@@ -0,0 +1,45 @@
+package Praktikum14Cucumber.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ContactPage extends BasePage{
+    @FindBy(xpath = "//h3[text()='Address']")
+    public WebElement address;
+
+    @FindBy(xpath = "//h3[text()='WhatsApp']")
+    public WebElement whatsApp;
+
+    @FindBy(xpath = "//h3[text()='Phone']")
+    public WebElement phone;
+
+    @FindBy(xpath = "//h3[text()='Email addresses']")
+    public WebElement emailAddresses;
+
+    @FindBy(xpath = "//div[@class=\"form-fields\"]")
+    public WebElement formFields;
+
+    @FindBy(xpath = "//label[text()='Ihr Name : ']")
+    public WebElement name;
+
+    @FindBy(xpath = "//label[text()='Ihre E-Mail : ']")
+    public WebElement email;
+
+    @FindBy(xpath = "//label[text()='Betreff : ']")
+    public WebElement theme;
+
+    @FindBy(xpath = "//label[text()='Anfrage : ']")
+    public WebElement request;
+
+    @FindBy(xpath = "//button[@name='send-email']")
+    public WebElement submitButton;
+
+
+
+}
--- src/test/resources/features/ContactFunctionality.feature
+++ src/test/resources/features/ContactFunctionality.feature
@@ -1,4 +1,3 @@
-
 Feature: Contact Methods
   This feature file contains scenarios to validate the functionality of the different contact methods on the website.
 
@@ -10,3 +9,33 @@ Feature: Contact Methods
   Open the Contact page through the main menu
     And The user click on the "Kontakt" in the navigation menu
     Then The user should be redirected to the contact page
+
+  Scenario Outline: Verify Contact Methods
+    And The user click on the "Kontakt" in the navigation menu
+    Then The user should see "<ContactMethods>" in the contact section
+    Examples:
+      | ContactMethods  |
+      | Address         |
+      | WhatsApp        |
+      | Phone           |
+      | Email addresses |
+      | form-fields     |
+
+  Scenario Outline: verify Contact Fields of  the Contact Form
+    And The user click on the "Kontakt" in the navigation menu
+    Then The user should see "<Contact Form>"
+    Examples:
+      | Contact Form  |
+      | Ihr Name :    |
+      | Ihre E-Mail : |
+      | Betreff :     |
+      | Anfrage :     |
+    And The form should include a Submit button
+
+
+
+
+
+
+
+
