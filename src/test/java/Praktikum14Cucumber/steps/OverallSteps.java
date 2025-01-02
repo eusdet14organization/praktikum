9734fc7bef63bf0223717f1cef109a33bbb53156
Ihor Tkachenko <igortkachenko987@gmail.com>
2025-01-02 18:15:00 +0100

    Final1

--- src/test/java/Praktikum14Cucumber/steps/OverallSteps.java
+++ src/test/java/Praktikum14Cucumber/steps/OverallSteps.java
@@ -6,9 +6,11 @@ import Praktikum14Cucumber.pages.MyAccountPage;
 import Praktikum14Cucumber.utils.ConfigurationReader;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
 import static Praktikum14Cucumber.context.TestContext.getActions;
+import static Praktikum14Cucumber.context.TestContext.getWait;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -33,6 +35,7 @@ public class OverallSteps {
         homePage.loginUser.click();
         accountPage.fillOutTheLogInFormWithData(email,password);
         getActions().moveToElement(accountPage.logInButton).click().perform();
+        getWait().until(ExpectedConditions.visibilityOf(accountPage.nameSubPage))
         assertTrue(accountPage.nameSubPage.isDisplayed());
         assertTrue(accountPage.titleH1.isDisplayed());
     }
